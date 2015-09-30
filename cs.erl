d939546ce9f30412270685bc6c0e469211bebfbd
tundi <tundi@bulbview.com>
2015-09-30 07:51:24 +0100

    spawn processes

--- cs.erl
+++ cs.erl
@@ -3,3 +3,10 @@
 
 ls() ->
     lists:seq(1,10).
+
+p() ->
+  spawn(fun()->io:format("~p~n",[2+2]) end).
+
+p2() ->
+  G = fun(X) -> timer:sleep(10), io:format("~p~n", [X]) end,
+  [spawn(fun() -> G(X) end) || X <- lists:seq(1,10)].
