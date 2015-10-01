8ca66ddc9745462e4b2500552766774a24c5aa79
tundi <tundi@bulbview.com>
2015-10-01 13:42:49 +0100

    type conversion BIF

--- cs.erl
+++ cs.erl
@@ -10,3 +10,6 @@ p() ->
 p2() ->
   G = fun(X) -> timer:sleep(10), io:format("~p~n", [X]) end,
   [spawn(fun() -> G(X) end) || X <- lists:seq(1,10)].
+
+typeconversion() -> binary_to_atom(<<"Erlang">>, utf8).
+% 'Erlang'
