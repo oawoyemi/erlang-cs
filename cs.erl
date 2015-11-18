5f331930d03556b993bceb2839f198ca3242fadc
tundi <tundi@bulbview.com>
2015-11-18 10:05:20 +0000

    updated

--- cs.erl
+++ cs.erl
@@ -5,47 +5,6 @@ console_print() ->
   io:format("~s~n", ["hello"]),
   io:format("~p~n", [[1, 2, 3]]).
 
-
-
-lists() ->
-  lists:seq(1, 10),
-  lists:map(fun(X) -> X * 10 end, [1, 2, 3]),  %[10,20,30]
-  lists:flatmap(fun(X) -> [X * 10] end, [1, 2, 3]).
-
-list_comps1() ->
-%%   NewList = [Expression || Pattern <- List, Condition1, Condition2, ... ConditionN]
-
-  [2 * N || N <- [1, 2, 3, 4]].
-%% [2,4,6,8]
-
-list_comp_filter() ->
-  [X || X <- [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], X rem 2 =:= 0].
-%% [2,4,6,8,10]
-
-list_comp_filter2() ->
-  %filtering generators
-  Weather = [{london, fog}, {paris, sun}, {boston, fog}, {vancouver, snow}],
-  FoggyPlaces = [X || {X, fog} <- Weather],
-  FoggyPlaces.
-%% [london,boston]
-
-list_comp_multiple_generators() ->
-  [X + Y || X <- [1, 2], Y <- [2, 3]].
-%%
-
-list_subtract() ->
-  A = lists:seq(1, 8),
-  B = lists:seq(1, 4),
-  C = lists:subtract(A, B),
-%% [5,6,7,8]
-  A -- B,
-%% [5,6,7,8]
-  [a, b, c, d] --[a, b, c],
-%% [d]
-  [a, b, c] -- [a, b, c, d].
-%% []
-
-
 processes() ->
   spawn(fun() -> io:format("~p~n", [2 + 2]) end).
 
